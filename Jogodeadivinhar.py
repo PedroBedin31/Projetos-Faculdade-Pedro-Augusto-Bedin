2e962daf5dc042631e04e668bcd4b9e01c03f842
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-08-25 08:36:21 -0300

    Jogo de adivinhar o número em PYTHON
    Feito por Pedro Augusto Bedin - 25/08/2026

---
+++ Jogodeadivinhar.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+print("----------------------ADIVINHE O NÚMERO----------------------")
+print("            REGRAS: Escolha um número de 1 a 100             ")
+print("O computador irá gerar um número aleatório, tente adivinha-lo")
+print("-------------------------------------------------------------")
+
+input("Clique qualquer botão para continuar... ")
+
+numero_secreto = random.randint(1, 100)
+
+palpite = int(input("Digite um número de 1 a 100 "))
+tentativas = 0
+
+while palpite != numero_secreto:
+    if palpite < numero_secreto:
+        print(f"ERRADO!! Tente um número MAIOR!!!")
+        palpite = int(input("Digite novamente "))
+        tentativas += 1
+    elif palpite > numero_secreto:
+        print(f"ERRADO!! Tente um número MENOR!!!")
+        palpite = int(input("Digite novamente "))
+        tentativas += 1
+
+if palpite == numero_secreto:
+    print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
+    print(f"Número secreto: {numero_secreto} ")
+    print(f"Tentativas: {tentativas}")
+
