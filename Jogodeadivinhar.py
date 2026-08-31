5eb6c08a0852855e149cbe3be181b02c9f3cb32f
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-08-30 21:53:52 -0300

    Fix difficulty selection and improve game prompts

--- Jogodeadivinhar.py
+++ Jogodeadivinhar.py
@@ -1,16 +1,24 @@
 import random
 import os
 
-print("----------------------ADIVINHE O NÚMERO----------------------")
-print("            REGRAS: Escolha um número de 1 a 100             ")
-print("O computador irá gerar um número aleatório, tente adivinha-lo")
+print("----------------------ADIVINHE O NÚMERO----------------------\n")
+print("O computador irá gerar um número aleatório, tente adivinha-lo\n")
 print("-------------------------------------------------------------")
 
-input("Clique o botão ENTER para continuar... ")
+print("            Dificuldade: Fácil (1 a 100)                      ")
+print("            Difculdade: Médio (1 a 500)                      ")
+print("            Difculdade: Difícil (1 a 1000)                   ")
 
-numero_secreto = random.randint(1, 100)
+dificuldade = int(input("Escolha a dificuldade: facil(1), medio(2) ou dificil(3): "))
 
-palpite = int(input("Digite um número de 1 a 100 "))
+if dificuldade == 1:
+    numero_secreto = random.randint(1, 50)
+elif dificuldade == 2:
+    numero_secreto = random.randint(1, 500)
+else:
+    numero_secreto = random.randint(1, 1000)
+
+palpite = int(input("Digite um número: "))
 tentativas = 1
 
 while palpite != numero_secreto:
@@ -27,4 +35,3 @@ if palpite == numero_secreto:
     print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
     print(f"Número secreto: {numero_secreto} ")
     print(f"Tentativas: {tentativas}")
-
