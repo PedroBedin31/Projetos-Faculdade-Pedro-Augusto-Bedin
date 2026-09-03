e51caca967d8d2d021623734c3c1648a3aac99c8
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-09-03 09:25:29 -0300

    Update Jogodeadivinhar.py

--- Jogodeadivinhar.py
+++ Jogodeadivinhar.py
@@ -5,8 +5,8 @@ print("----------------------ADIVINHE O NÚMERO----------------------\n")
 print("O computador irá gerar um número aleatório, tente adivinha-lo\n")
 print("-------------------------------------------------------------")
 
-print("            Dificuldade: Fácil (1 a 100), Tentativas: 6                  ")  
-print("            Difculdade: Médio (1 a 500), Tentativas: 8                   ")
+print("            Dificuldade: Fácil (1 a 100), Tentativas: 8                  ")
+print("            Difculdade: Médio (1 a 500), Tentativas: 9                   ")
 print("            Difculdade: Difícil (1 a 1000), Tentativas: 10               ")
 print("            Digite o número da dificuldade desejada:                     ")
 dificuldade = int(input("Escolha a dificuldade: facil(1), medio(2) ou dificil(3): "))
@@ -26,15 +26,15 @@ while palpite != numero_secreto:
         print(f"ERRADO!! Tente um número MAIOR!!!")
         palpite = int(input("Digite novamente "))
         tentativas += 1
-        if dificuldade == 1 and tentativas > 6:
+        if dificuldade == 1 and tentativas > 8:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
             print(f"Número secreto: {numero_secreto} ")
             print(f"Tentativas: {tentativas}")
             break
-        elif dificuldade == 2 and tentativas > 8:
+        elif dificuldade == 2 and tentativas > 9:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
             print(f"Número secreto: {numero_secreto} ")
-            print(f"Tentativas: {tentativas}") 
+            print(f"Tentativas: {tentativas}")
             break
         elif dificuldade == 3 and tentativas > 10:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
@@ -45,23 +45,39 @@ while palpite != numero_secreto:
         print(f"ERRADO!! Tente um número MENOR!!!")
         palpite = int(input("Digite novamente "))
         tentativas += 1
-        if dificuldade == 1 and tentativas > 6:
+        if dificuldade == 1 and tentativas > 8:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
             print(f"Número secreto: {numero_secreto} ")
             print(f"Tentativas: {tentativas}")
             break
-        elif dificuldade == 2 and tentativas > 8:
+        elif dificuldade == 2 and tentativas > 9:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
             print(f"Número secreto: {numero_secreto} ")
-            print(f"Tentativas: {tentativas}") 
+            print(f"Tentativas: {tentativas}")
             break
         elif dificuldade == 3 and tentativas > 10:
             print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
             print(f"Número secreto: {numero_secreto} ")
             print(f"Tentativas: {tentativas}")
             break
-
-if palpite == numero_secreto:
-    print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
-    print(f"Número secreto: {numero_secreto} ")
-    print(f"Tentativas: {tentativas}")
+    if palpite == numero_secreto:
+        if dificuldade == 1 and tentativas <= 8:
+            print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
+        elif dificuldade == 2 and tentativas <= 9:
+            print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
+        elif dificuldade == 3 and tentativas <= 10:
+            print("---------- PARABÉNS, VOCÊ GANHOU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
+        else:
+            print("---------- FIM DE JOGO, VOCÊ PERDEU!!! ----------")
+            print(f"Número secreto: {numero_secreto} ")
+            print(f"Tentativas: {tentativas}")
+            break
